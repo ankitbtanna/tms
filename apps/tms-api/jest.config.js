308626d525b2d8ebf09e277e02a6bfe64671edc6
Ankit Tanna <ankittanna@hotmail.com>
2020-11-18 21:24:00 +0530

    Initial commit of nxnrwl setup

---
+++ apps/tms-api/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'tms-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/tms-api',
+};
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/apps/tms'],
+  projects: ['<rootDir>/apps/tms', '<rootDir>/apps/tms-api'],
 };
