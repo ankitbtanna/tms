10bc00de05c0823eadbc5375042e2c24953cacf8
Ankit Tanna <ankittanna@hotmail.com>
2020-11-19 18:59:58 +0530

    [Navigation] | Add Navigation Menu
    Added angular flex-layout, useful tool
    
    used mat nav bar and side nav list

--- apps/tms/src/app/material.module.ts
+++ apps/tms/src/app/material.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from "@angular/core";
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+const MAT_MODULES = [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    FlexLayoutModule
+]
 
 @NgModule({
-    imports: [MatToolbarModule],
-    exports: [MatToolbarModule]
+    imports: MAT_MODULES,
+    exports: MAT_MODULES
 })
 export class MaterialModule {}
--- apps/tms/src/app/navbar/navbar.component.ts
+++ apps/tms/src/app/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'tms-workspace-navbar',
+  // tslint:disable-next-line: component-selector
+  selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @Output() sidenavToggle = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onToggleSideNav(): void {
+    this.sidenavToggle.emit();
   }
 
 }
