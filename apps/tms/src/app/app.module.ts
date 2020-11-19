19ed1988aa2fd83e95e3d2d27fa50735d19dccfd
Ankit Tanna <ankittanna@hotmail.com>
2020-11-19 11:49:25 +0530

    [Material Module] | Integrate a material module

--- apps/tms/src/app/app.module.ts
+++ apps/tms/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginModule } from './login/login.module';
 import { AppRoutingModule } from './app.routing.module';
+import { MaterialModule } from './material.module';
+import { NavbarComponent } from './navbar/navbar.component'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, LoginModule],
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule, BrowserAnimationsModule, LoginModule],
   providers: [],
   bootstrap: [AppComponent],
 })
