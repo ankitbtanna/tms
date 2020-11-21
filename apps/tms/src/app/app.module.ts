dab88123b15e9fcd7ef658278de8a2877d0d0977
Ankit Tanna <ankittanna@hotmail.com>
2020-11-22 00:56:26 +0530

    [Dashboard] | Add Dashboard Module

--- apps/tms/src/app/app.module.ts
+++ apps/tms/src/app/app.module.ts
@@ -7,11 +7,12 @@ import { LoginModule } from './login/login.module';
 import { AppRoutingModule } from './app.routing.module';
 import { MaterialModule } from './material.module';
 import { NavbarComponent } from './navbar/navbar.component';
-import { RegisterModule } from './register/register.module'
+import { RegisterModule } from './register/register.module';
+import { DashboardModule } from './dashboard/dashboard.module'
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, MaterialModule, BrowserAnimationsModule, LoginModule, RegisterModule],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule, BrowserAnimationsModule, LoginModule, RegisterModule, DashboardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/tms/src/app/app.routes.ts
+++ apps/tms/src/app/app.routes.ts
@@ -1,21 +1,31 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
 
 export const TMS_ROUTES: Routes = [
-    {
-        path: '',
-        redirectTo: '/login', 
-        pathMatch: 'full'
-    },
-    {
-        path: 'login',
-        loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
-    },
-    {
-        path: 'forgot-password',
-        loadChildren: () => import('./forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule)
-    },
-    {
-        path: 'register',
-        loadChildren: () => import('./register/register.module').then(m => m.RegisterModule)
-    }
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () =>
+      import('./forgot-password/forgot-password.module').then(
+        (m) => m.ForgotPasswordModule
+      ),
+  },
+  {
+    path: 'register',
+    loadChildren: () =>
+      import('./register/register.module').then((m) => m.RegisterModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
 ];
