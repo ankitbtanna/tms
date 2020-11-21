79422fa6acd18c40b13b663c3e6a4d95c46d6e8c
Ankit Tanna <ankittanna@hotmail.com>
2020-11-22 00:38:55 +0530

    [Forgot Password] | Create Forgot Password Screen
    Created forgot password screen and started using FX Flex module of Angular

--- apps/tms/src/app/forgot-password/forgot-password.component.ts
+++ apps/tms/src/app/forgot-password/forgot-password.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { USERNAME_REGEX, PASSWORD_REGEX } from './forgot-password.constant';
 
 @Component({
   selector: 'tms-workspace-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent implements OnInit {
+  hide = true;
+  isPasswordFocused = false;
+  isConfirmPasswordFocused = false;
+  forgotPasswordForm: FormGroup = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.pattern(USERNAME_REGEX),
+    ]),
+  });
 
-  constructor() { }
+  passwordForm = this.formBuilder.group(
+    {
+      password: ['', [Validators.required, Validators.pattern(PASSWORD_REGEX)]],
+      confirmPassword: [
+        '',
+        [Validators.required, Validators.pattern(PASSWORD_REGEX)],
+      ],
+    },
+    { validator: this.checkPasswords }
+  );
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  checkPasswords(group: FormGroup) {
+    const pass = group.controls.password.value;
+    const confirmPass = group.controls.confirmPassword.value;
+    return pass === confirmPass ? null : { notSame: true };
   }
-
 }
