6a01ff023da8b5c8d6aaf04ac5c384007b679dd7
Ankit Tanna <ankittanna@hotmail.com>
2020-12-06 00:17:13 +0530

    [Share TMS] | Add Share TMS form

--- apps/tms/src/app/share-tms/share-tms.module.ts
+++ apps/tms/src/app/share-tms/share-tms.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ShareTmsRoutingModule } from './share-tms-routing.module';
 import { ShareTmsComponent } from './share-tms/share-tms.component';
+import { MaterialModule } from '../material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ShareTmsComponent],
-  imports: [CommonModule, ShareTmsRoutingModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    ShareTmsRoutingModule,
+  ],
 })
 export class ShareTmsModule {}
--- apps/tms/src/app/share-tms/share-tms/share-tms.component.ts
+++ apps/tms/src/app/share-tms/share-tms/share-tms.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MOBILE_NUMBER_REGEX } from '../../register/register.constant';
+import { DESCRIPTION_REGEX, NAME_REGEX } from '../share-tms.constant';
 
 @Component({
   // tslint:disable-next-line: component-selector
   selector: 'tms-share-tms',
   templateUrl: './share-tms.component.html',
   styleUrls: ['./share-tms.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ShareTmsComponent implements OnInit {
+  shareTMSForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.pattern(NAME_REGEX),
+    ]),
+    mobileNumber: new FormControl('', [
+      Validators.required,
+      Validators.pattern(MOBILE_NUMBER_REGEX),
+    ]),
+    description: new FormControl('', [Validators.pattern(DESCRIPTION_REGEX)]),
+  });
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  addStakeHolder() {}
+
+  resetShareTMSForm() {
+    this.shareTMSForm.reset();
+  }
 }
