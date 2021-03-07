0ae0e96a2855af7c0079321046f56a0ac8963dfd
Ankit Tanna <ankittanna@hotmail.com>
2021-03-07 21:40:53 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,25 +1,8 @@
-"use strict";
-const express = require("express");
-const compression = require("compression");
-
-const _port = 9000;
-const _app_folder = 'dist/apps/tms';
-
+const express = require('express');
+const path = require('path');
 const app = express();
-app.use(compression());
-
-
-// ---- SERVE STATIC FILES ---- //
-app.server.get('*.*', express.static(_app_folder));
-
-// ---- SERVE APLICATION PATHS ---- //
-app.all('*', function (req, res) {
-  res.status(200).sendFile(`dist/apps/tms/index.html`, {
-    root: _app_folder
-  });
-});
-
-// ---- START UP THE NODE SERVER  ----
-app.listen(_port, function () {
-  console.log("Node Express server for " + app.name + " listening on http://localhost:" + _port);
-});
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/apps/tms', 'index.html'));
+})
+app.listen(9000);
